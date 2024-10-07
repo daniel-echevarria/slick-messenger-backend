f6881b9187c7f810385703a1ad8dbe78539374be
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-10-07 11:53:34 +0200

    Add conversation routes and simplify users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+
   namespace :auth do
     post 'google', to: 'auth#google'
   end
-  resources :users
-  devise_for :users, path: '',
-  controllers: {
+
+  get 'users', to: 'users#index'
+
+  resources :conversations
+
+  devise_for :users, path: '', controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
