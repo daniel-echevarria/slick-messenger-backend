cc61c363e9c5bb09773f349b6c4965e2d8cdd46e
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-10-17 16:15:51 +0200

    Add a workaround for profile creation with google login

--- app/controllers/auth/auth_controller.rb
+++ app/controllers/auth/auth_controller.rb
@@ -7,30 +7,29 @@ module Auth
       token = params[:token]
       response = Net::HTTP.get_response(URI("https://oauth2.googleapis.com/tokeninfo?id_token=#{token}"))
 
-      p "this is the response from google #{response}"
 
       if response.is_a?(Net::HTTPSuccess)
         user_info = JSON.parse(response.body)
+        p "this is the user info #{user_info}"
         user = User.find_or_create_by(email: user_info['email']) do |u|
           u.password = Devise.friendly_token[0, 20]
           u.name = user_info['name']
           u.avatar_url = user_info['picture']
         end
 
-        profile = Profile.find_or_create_by(user_id: user.id) do |p|
-          p.display_name = user_info['name']
-          p.name = user_info['name']
-          p.picture = user_info['picture']
-          p.email = user_info['email']
-        end
-
-        p "this is the user #{user.email}"
+        profile = Profile.find_by(user_id: user.id)
+        profile.update(
+          display_name: user_info['name'],
+          name: user_info['name'],
+          picture: user_info['picture'],
+          email: user_info['email']
+        )
 
         if user.persisted?
           jwt = Warden::JWTAuth::UserEncoder.new.call(user, :user, nil).first
           render json: { jwt: jwt }, status: :ok
         else
-          render json: { error: 'Could not find the user with this email' }, status: 404
+          render json: { error: 'Could not find the user with this email' }, status: :not_found
         end
       else
         render json: { error: 'Invalid token' }, status: :unauthorized
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_one :profile
+
+  after_create :add_profile
+
   include Devise::JWT::RevocationStrategies::JTIMatcher
 
   devise :database_authenticatable,
@@ -7,4 +11,15 @@ class User < ApplicationRecord
          :validatable,
          :jwt_authenticatable,
          jwt_revocation_strategy: self
+
+  private
+
+  def add_profile
+    unless self.profile
+      self.create_profile do |p|
+        p.email = self.email
+        p.picture = 'generic profile image url'
+      end
+    end
+  end
 end
