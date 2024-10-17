5a96f968664bbdcca58b2fdedb9a63cae040ed62
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-10-17 11:34:13 +0200

    Add profile creation to google auth process

--- app/controllers/auth/auth_controller.rb
+++ app/controllers/auth/auth_controller.rb
@@ -7,6 +7,8 @@ module Auth
       token = params[:token]
       response = Net::HTTP.get_response(URI("https://oauth2.googleapis.com/tokeninfo?id_token=#{token}"))
 
+      p "this is the response from google #{response}"
+
       if response.is_a?(Net::HTTPSuccess)
         user_info = JSON.parse(response.body)
         user = User.find_or_create_by(email: user_info['email']) do |u|
@@ -15,14 +17,21 @@ module Auth
           u.avatar_url = user_info['picture']
         end
 
+        profile = Profile.find_or_create_by(user_id: user.id) do |p|
+          p.display_name = user_info['name']
+          p.name = user_info['name']
+          p.picture = user_info['picture']
+          p.email = user_info['email']
+        end
+
         p "this is the user #{user.email}"
 
         if user.persisted?
           jwt = Warden::JWTAuth::UserEncoder.new.call(user, :user, nil).first
           render json: { jwt: jwt }, status: :ok
+        else
+          render json: { error: 'Could not find the user with this email' }, status: 404
         end
-
-        # Generate a JWT token
       else
         render json: { error: 'Invalid token' }, status: :unauthorized
       end
