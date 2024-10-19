611a15312e64aa06c979a6047a4f4a3d36501237
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-10-19 18:05:13 +0200

    Implement profile updating

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,5 +5,19 @@ class ProfilesController < ApplicationController
   end
 
   def update
+    profile = Profile.find(params[:id])
+    profile.update(profile_params)
+
+    if profile.save
+      render json: { profile: profile, message: 'Profile updated :)' }, status: :ok
+    else
+      render json: { message: 'Problemo when updating the profile' }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def profile_params
+    params.require(:profile).permit(:name, :display_name, :picture, :title)
   end
 end
