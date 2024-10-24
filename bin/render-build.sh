5023f2890fb22bd05199f663c9f6e28f7e0882d5
Kasper Bak <102696643+SpookyKasper@users.noreply.github.com>
2024-10-24 16:47:08 +0200

    remove assets commands from build file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,8 +3,6 @@
 set -o errexit
 
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
 bundle exec rails db:migrate
 
 # If you're using a Free instance type, you need to
